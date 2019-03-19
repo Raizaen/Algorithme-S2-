76eedda19a6725b600b8b70c1587ef74e69bddc3
Raizaen <31708305+Raizaen@users.noreply.github.com>
2019-03-19 15:55:29 +0100

    Add files via upload

--- TP2019/TP2-Exo1.cpp
+++ TP2019/TP2-Exo1.cpp
@@ -29,17 +29,16 @@ struct ensperson {
   
 };
 
-/*struct enspersonref {
-    tabp ref_person;
-    int nbr_person;
-  
+struct enspersonref {
+  tabp ref_person;
+  int nbr_person;
+
 };
-*/
 
 //------------------------------------------------------- AFFICHAGE
 
 void affich (person p) {
-    cout << "Nom : " << p.lastname << ", Prénom : " << p.firstname << ", Date de naissance : " << p.date.day << "/" << p.date.month << "/" << p.date.year << endl;
+  cout << "Nom : " << p.lastname << ", Prénom : " << p.firstname << ", Date de naissance : " << p.date.day << "/" << p.date.month << "/" << p.date.year << endl;
 }
 
 void affichfull (ensperson p, int n) {
@@ -47,16 +46,27 @@ void affichfull (ensperson p, int n) {
     affich(p.a[i]);
   }
 }
+/*
+void affichref (person* p) {
+  cout << "Nom : " << p->lastname << ", Prénom : " << p->firstname << ", Date de naissance : " << p->date.day << "/" << p->date.month << "/" << p->date.year << endl;
+}
+
+void affichfullref (enspersonref p, int n) {
+  for(int i=0;i<n;i++){
+    affichref(p.ref_person[i]);
+  }
+}
+*/
 
 //------------------------------------------------------- SAISIE
 
 void saisie (person & p) {
   cout << "Nom : ";
-  cin >> p.lastname;
+    cin >> p.lastname;
   cout << "Prénom : ";
-  cin >> p.firstname;
+    cin >> p.firstname;
   cout << "Date de naissance (jour mois année) : ";
-  cin >> p.date.day >> p.date.month >> p.date.year;
+    cin >> p.date.day >> p.date.month >> p.date.year;
   cout << endl;
 }
 
@@ -67,9 +77,30 @@ void saisiefull (ensperson & p, int & n) {
   }
 }
 
+void init_ensref (tabperson p, int n , tabp &m) {
+  for (int i=0; i < n; i++) {
+    m[i] = &p[i];
+  }
+}
+
+void affichref (tabp m, int n) {
+  for (int i=0; i<n; i++) {
+    cout << "Nom : " << m[i]->lastname << ", Prénom : " << m[i]->firstname << ", Date de naissance : " << m[i]->date.day << "/" << m[i]->date.month << "/" << m[i]->date.year << endl;
+  }
+}
+
+void init_ensref_date (tabperson p, int n , tabp &m, int date) {
+  for (int i=0; i < n; i++) {
+    if (date == p[i].date.year) {
+      m[i] = &p[i];
+    }
+  }
+}
+
 int main () {
   person a;
   ensperson p;
+  enspersonref m,o;
   
   int n = 0;
   //cout << "Combien de personne voulez vous rentrer ? : ";
@@ -77,7 +108,14 @@ int main () {
   //cout <<  endl;
   
   saisiefull(p,n);
+  init_ensref(p.a,n,m.ref_person);
   //affich(a, n);
   affichfull(p,n);
+  cout << endl;
+  affichref(m.ref_person,n);
+  cout<< endl;
+  init_ensref_date(p.a,n,o.ref_person,1999);
+  affichref(o.ref_person,n);
+  
   return 0;  
 }
